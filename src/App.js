cbfc80a1f26ebfa63222e5c370134bb1fb450187
Marvin Cespedes <marvin.cespedes@benchmarkanalytics.com>
2020-01-07 18:02:21 -0600

    end of 22 - array of monsters, dynamic content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,20 +6,29 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Hello Marvin"
+      monsters: [
+        {
+          name: "Frankenstein",
+          id: "asd"
+        },
+        {
+          name: "Dracula",
+          id: "fgh"
+        },
+        {
+          name: "Zombie",
+          id: "jkl"
+        }
+      ]
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "Ahoy Marvo!" })}>
-            change text
-          </button>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
