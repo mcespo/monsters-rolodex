a0750589d7d0b69cb7d74ae818a5011b3d430456
Marvin Cespedes <marvin.cespedes@benchmarkanalytics.com>
2020-01-27 17:55:25 -0600

    Updates

---
+++ precache-manifest.1b5d16ca424999c2424f4894576bc191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b43bc61d460e73451a9e8a21ae065d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "88a22d5bebeef909070a",
+    "url": "/monsters-rolodex/static/css/main.8c9bffc9.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88a22d5bebeef909070a",
+    "url": "/monsters-rolodex/static/js/main.3061de35.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
